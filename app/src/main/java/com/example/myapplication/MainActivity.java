1992e2b1bae8b2e115d33c86b3c72133ad1db301
Bautimp <bautistamontesp@gmail.com>
2026-05-10 17:17:01 -0300

    Implementación de operación módulo y símbolo según la operación seleccionada

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -13,25 +13,28 @@ import androidx.core.view.WindowInsetsCompat;
 import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
     private EditText Valor1, Valor2;
-    private TextView resultado;
+    private TextView resultado, textViewOperacion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.layout4);
-         Valor1 = (EditText) findViewById(R.id.editTextNumber);
+        Valor1 = (EditText) findViewById(R.id.editTextNumber);
         Valor2 = (EditText) findViewById(R.id.editTextNumber2);
         resultado = (TextView) findViewById(R.id.editTextNumber3);
-        
-
+        textViewOperacion = (TextView) findViewById(R.id.textViewOperacion);
     }
 
+    public void setSimbolo(View view, String simbolo) {
+        textViewOperacion.setText(simbolo);
+    }
     public void operacionesSuma(View view) {
         int valor1 = Integer.parseInt(Valor1.getText().toString());
         int valor2 = Integer.parseInt(Valor2.getText().toString());
         int operacion = valor1 + valor2;
         String result = String.valueOf(operacion);
+        setSimbolo(view, "+");
         resultado.setText(result);
     }
     public void operacionesResta(View view) {
@@ -39,6 +42,7 @@ public class MainActivity extends AppCompatActivity {
         int valor2 = Integer.parseInt(Valor2.getText().toString());
         int operacion = valor1 - valor2;
         String result = String.valueOf(operacion);
+        setSimbolo(view, "-");
         resultado.setText(result);
     }
 
@@ -47,6 +51,7 @@ public class MainActivity extends AppCompatActivity {
         int valor2 = Integer.parseInt(Valor2.getText().toString());
         int operacion = valor1 * valor2;
         String result = String.valueOf(operacion);
+        setSimbolo(view, "*");
         resultado.setText(result);
     }
     public void operacionesDivision(View view) {
@@ -55,11 +60,22 @@ public class MainActivity extends AppCompatActivity {
         if(valor2 != 0) {
             int operacion = valor1 / valor2;
             String result = String.valueOf(operacion);
+            setSimbolo(view, "/");
             resultado.setText(result);
         }else{
             Toast.makeText(this, "No se puede dividir por 0", Toast.LENGTH_SHORT).show();
         }
     }
+
+    public void operacionesModulo(View view) {
+        int valor1 = Integer.parseInt(Valor1.getText().toString());
+        int valor2 = Integer.parseInt(Valor2.getText().toString());
+        int operacion = valor1 % valor2;
+        String result = String.valueOf(operacion);
+        setSimbolo(view, "%");
+        resultado.setText(result);
+    }
+
 
 
 
